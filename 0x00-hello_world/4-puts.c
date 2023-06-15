5f259eacec114412c478fe3f7c72373b2c58496d
walid oumechtak <woumechtak@gmail.com>
2023-06-15 01:13:55 -0700

    first program in c that print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the start point of the program
+ * return: 0 if success or !0 if not
+ */
+
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
+}
