81888209ba5dc031c03a99cdea46e6f6ca5772d5
walid oumechtak <woumechtak@gmail.com>
2023-06-15 05:19:29 -0700

    got it

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
  *main - the starting point of any program in c
  * Return: 0 if success or !0 if fail
  */
-
 int main(void)
 {
 	char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	write(2, str, 58);
 	write(2, "\n", 1);
-	return (0);
+	return (1);
 }
