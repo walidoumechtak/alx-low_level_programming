a65bedfefd8749b057493ace123d7dd91fb1ef26
walid oumechtak <woumechtak@gmail.com>
2023-06-15 01:22:18 -0700

    got

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - the start point of the program
+ * Return: 0 if success or !0 if not
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
