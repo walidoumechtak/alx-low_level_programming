2247e74834937ad6fd3db27b52d14df689d1ff6a
walid oumechtak <woumechtak@gmail.com>
2023-06-15 05:11:07 -0700

    qutoe

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ *main - the starting point of any program in c
+ * Return: 0 if success or !0 if fail
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 50);
+}
