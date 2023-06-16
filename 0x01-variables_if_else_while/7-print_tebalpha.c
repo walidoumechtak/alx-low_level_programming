bb6f13e8e22461edf0d3baec867c7231e2b5b3bb
walid oumechtak <woumechtak@gmail.com>
2023-06-16 08:18:39 -0700

    go exe 7

--- 0x01-variables_if_else_while/7-print_tebalpha.c
+++ 0x01-variables_if_else_while/7-print_tebalpha.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
-
-/**
- * main - the starting point of every program
- * Return: 0 if success or !0 if fail
- */
-
-int main(void)
-{
-	int     s;
-
-	s = 'z';
-	while (s <= 'a')
-		putchar(s--);
-	putchar('\n');
-	return (0);
-}
+#
