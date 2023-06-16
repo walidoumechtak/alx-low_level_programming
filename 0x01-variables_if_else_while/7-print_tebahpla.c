87fe07c1dbdc5af1301cb1a1f051f06062bf2b20
walid oumechtak <woumechtak@gmail.com>
2023-06-16 08:21:47 -0700

    ok done with 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
-#
+#include <stdio.h>
+
+/**
+ * main - the start of the prog
+ * Return: 0 success !0 fail
+ */
+
+int main(void)
+{
+	int	s;
+
+	s = 'z';
+	while (s >= 'a')
+		putchar(s--);
+	putchar('\n');
+	return (0);
+}
