a2608595bc1eb4ee68861a9287e7ddefa5d08e50
walid oumechtak <woumechtak@gmail.com>
2023-06-20 13:42:33 -0700

    done with 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all minit starting from 00:00 to 23:59
+ */
+
+void	_putnbr(int n)
+{
+	if (n >= 10)
+		_putnbr(n / 10);
+	_putchar(n % 10 + '0');
+}
+
+void	jack_bauer(void)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 23)
+	{
+		j = 0;
+		while (j <= 59)
+		{
+			if (i >= 0 && i <= 9)
+			{
+				_putchar('0');
+				_putchar(i + '0');
+			}
+			else
+				_putnbr(i);
+			_putchar(':');
+			if (j >= 0 && j <= 9)
+			{
+				_putchar('0');
+				_putchar(j + '0');
+			}
+			else
+				_putnbr(j);
+			_putchar('\n');
+			j++;
+		}
+		i++;
+	}
+}
