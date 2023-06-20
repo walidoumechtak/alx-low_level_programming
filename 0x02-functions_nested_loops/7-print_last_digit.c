bcf113ec6931810a9dcbb83f5262570d7d70f170
walid oumechtak <woumechtak@gmail.com>
2023-06-20 02:54:25 -0700

    correcte 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,10 @@ int	print_last_digit(int n)
 {
 	int	last;
 
-	if (n == –2147483648)
-	{
-		last = -(n % 10);
-		_putchar(last + '0');
-		return (last);
-	}
-	if (n < 0)
-		n *= -1;
+
 	last = n % 10;
+	if (n < 0)
+		last = - (n % 10);
 	_putchar(last + '0');
 	return (last);
 }
