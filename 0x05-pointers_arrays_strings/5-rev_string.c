15761383ff9ee1e9fbbc6d9f4c6278dcd1dffbe3
walid oumechtak <woumechtak@gmail.com>
2023-06-26 04:59:02 -0700

    done with exe 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - funct that return the length of a string
+ * @s: the sting
+ * Return: the length
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
+ * rev_string - funct that rev a string
+ * @s: the string should be reversed
+ */
+
+void	rev_string(char *s)
+{
+	int	start;
+	int	end;
+	char	temp;
+
+	start = 0;
+	end = _strlen(s) - 1;
+	while (start != end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
+}
