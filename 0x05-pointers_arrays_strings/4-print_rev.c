6149801a61119b6284bda6f3266e47f8459a54a8
walid oumechtak <woumechtak@gmail.com>
2023-06-26 04:31:12 -0700

    correcte reve line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - len of string
+ * @s: the string
+ * Return: length
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * print_rev - functin that  print string in reverse mode
  * @s: the sting
  */
