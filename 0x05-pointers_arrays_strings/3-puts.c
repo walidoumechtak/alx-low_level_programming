034cc54cf066104b844e10b42a4ab2678a01bb04
walid oumechtak <woumechtak@gmail.com>
2023-06-26 04:04:53 -0700

    done with strlen

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - function that puts a stirng in standard output
+ * @str: the string should be puted
+ */
+
+void	_puts(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,8 +1,12 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 
+#include <stdlib.h>
+#include <unistd.h>
 void reset_to_98(int *n);
 void	swap_int(int *a, int *b);
 int	_strlen(char *s);
+void	_puts(char *str);
+
 
 #endif /* MAIN_H*/
