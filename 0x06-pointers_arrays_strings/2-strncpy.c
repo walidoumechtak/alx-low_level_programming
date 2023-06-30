924a9e57125eed1b9cf5f4c6b12412ee3134c84d
walid oumechtak <woumechtak@gmail.com>
2023-06-30 02:58:01 -0700

    done strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy n byte from src to dest
+ * @dest: dest buffer
+ * @src: src string
+ * @n: n bytes
+ * Return: dest string (new string)
+ */
+
+char	*_strncpy(char *dest, char *src, int n)
+{
+	int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
