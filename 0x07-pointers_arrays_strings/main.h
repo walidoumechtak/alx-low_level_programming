f33357a89091907b36984d281a1d4189830a84de
walid oumechtak <woumechtak@gmail.com>
2023-07-04 03:24:58 -0700

    go

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif /* _MAIN_H_ */
