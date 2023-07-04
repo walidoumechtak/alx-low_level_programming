10b87bdc70424fc73dced68deed97ee68ee62f16
walid oumechtak <woumechtak@gmail.com>
2023-07-04 03:21:24 -0700

    go

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - search for the first occurence in a string
+ * @s: the string
+ * @c: the charactere
+ * Return: the firt occurence of c in s
+ */
+
+char	*_strchr(char *s, char c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (*s == c)
+			return (s);
+		*s++;
+	}
+	return (NULL);
+}
