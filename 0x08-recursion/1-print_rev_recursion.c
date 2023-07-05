2b9e953b3e94a501d34db1441e1a85786886cd8b
walid oumechtak <woumechtak@gmail.com>
2023-07-05 03:21:56 -0700

    done with the exe

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that print a string in reverse with recursion
+ * @s: the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_print_rev_recursion(s + 1);
+	write(1, &s[0], 1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,5 +6,6 @@
 #include <stdlib.h>
 
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* _MAIN_H_ */
