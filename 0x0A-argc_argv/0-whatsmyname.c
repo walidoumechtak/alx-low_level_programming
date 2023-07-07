8e341d2ad96c13614364f87ce97582ba15f619e8
walid oumechtak <woumechtak@gmail.com>
2023-07-07 03:27:11 -0700

    done with exe0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - the start of the program
+ * @ac: the size or av
+ * @av: **arr of arguments
+ * Return: allways 0
+ */
+
+int main(int ac, char **av)
+{
+	printf("%s\n", av[0]);
+	return (0);
+}
