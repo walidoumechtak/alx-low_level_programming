34e6f027a117dc71ad47b497798698086e47b84a
walid oumechtak <woumechtak@gmail.com>
2023-07-10 03:58:48 -0700

    done with exe

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * str_concat - concate two string in a single buffer
+ * @s1: the first string
+ * @s2: the second string
+ * Return: the concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int	i;
+	int	len;
+	char	*res;
+	int	j;
+
+	if (!s1)
+		return (s2);
+	if (!s2)
+		return (s1);
+	len = strlen(s1) + strlen(s2);
+	res = malloc((len + 1) * sizeof(char));
+	i = 0;
+	j = 0;
+	while (s1[i])
+		res[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		res[j++] = s2[i++];
+	res[j] = '\0';
+	return (res);
+}
