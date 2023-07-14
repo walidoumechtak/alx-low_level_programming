619e41364d2df8b40576f75ef225c593893d2a1d
walid oumechtak <woumechtak@gmail.com>
2023-07-14 04:00:47 -0700

    done with the exe

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * array_range - create array conataine from min to max ordered
+ * @min: the min number
+ * @max: the max number
+ * Return: pointer to the array
+ */
+
+int *array_range(int min, int max)
+{
+	int	*arr;
+	int	size;
+	int	i;
+
+	if (min > max)
+		return (NULL);
+	size = max - min;
+	arr = malloc(size * sizeof(int));
+	if (!arr)
+		return (NULL);
+	i = 0;
+	while (min <= max)
+		arr[i++] = min++;
+	return (arr);
+}
