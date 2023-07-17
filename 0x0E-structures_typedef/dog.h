81b42cab9054caf8421f9fc959a3a5e509dace08
walid oumechtak <woumechtak@gmail.com>
2023-07-17 01:38:23 -0700

    go

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,6 +16,12 @@ struct dog
 	char	*owner;
 };
 
+/**
+ * dog_t - Typdef of dog struct
+ */
+
+typedef dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
