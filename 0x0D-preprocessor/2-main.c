cf73ae80e84763b30e3b1aec22eed7bc6d4851d9
walid oumechtak <woumechtak@gmail.com>
2023-07-17 00:17:58 -0700

    go

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define FILE_NAME "2-main.c"
+
+/**
+ * main - the start of the program
+ * Return: allways 0
+ */
+
+int main(void)
+{
+	printf("%s\n", FILE_NAME);
+	return (0);
+}
