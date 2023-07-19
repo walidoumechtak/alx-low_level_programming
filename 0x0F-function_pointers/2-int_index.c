dcdfdc3e5aef500ad4da8ce9a8a9e025911dbc6b
walid oumechtak <woumechtak@gmail.com>
2023-07-19 00:49:44 -0700

    go

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - the index of the first element match
+ * @array: the givern array
+ * @size: the size of the array
+ * @cmp: the function pointer
+ * Return: the index of the match element
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	size_t	i;
+
+	if (size <= 0)
+		return (-1);
+	while (i < size)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+		i++;
+	}
+	return (-1);
+}
