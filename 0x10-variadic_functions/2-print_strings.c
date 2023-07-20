550dd7c6625aca53a29ef9f522686e32d1d7b054
walid oumechtak <woumechtak@gmail.com>
2023-07-20 12:21:48 -0700

    done with the exe

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,12 +10,17 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list lst;
 	unsigned int    i;
+	char	*temp;
 
 	i = 0;
 	va_start(lst, n);
 	while (i < n)
 	{
-		printf("%s", va_arg(lst, char *));
+		temp = va_arg(lst, char *);
+		if (!temp)
+			printf("(nil)");
+		else
+			printf("%s", temp);
 		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 		i++;
