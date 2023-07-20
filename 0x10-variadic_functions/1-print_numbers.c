be4604f0be7226c889c96a99fe07409aee1e0660
walid oumechtak <woumechtak@gmail.com>
2023-07-20 12:15:16 -0700

    done with the exe

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -16,7 +16,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		printf("%d", va_arg(lst, int));
-		if (separator != NULL)
+		if (separator != NULL && i < n - 1)
 			printf("%s", separator);
 		i++;
 	}
