57fd893d36ec7b1b7535f2c9c245c4055c82785b
walid oumechtak <woumechtak@gmail.com>
2023-07-20 12:13:42 -0700

    done with the exe

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - print all number passed as argument
+ * @separator: the char that should printed between numbers
+ * @n: the size of arguments
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lst;
+	unsigned int	i;
+
+	i = 0;
+	va_start(lst, n);
+	while (i < n)
+	{
+		printf("%d", va_arg(lst, int));
+		if (separator != NULL)
+			printf("%s", separator);
+		i++;
+	}
+	printf("\n");
+}
