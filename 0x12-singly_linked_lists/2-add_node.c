ffa8b7406f4d9c2f35c7f421ed6877af815bade9
walid oumechtak <woumechtak@gmail.com>
2023-07-27 02:25:17 -0700

    done with the exe

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,7 +30,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t	*new;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (!new || !str)
 		return (NULL);
 	new->str = strdup(str);
 	new->len = _strlen(str);
