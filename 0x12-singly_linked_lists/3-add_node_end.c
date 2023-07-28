d6d6b18a9a584398ff30673a3c232167b1c09140
walid oumechtak <woumechtak@gmail.com>
2023-07-28 07:33:06 -0700

    correct 3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -26,7 +26,7 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t	*new;
 	list_t	*temp;
 
-	new = malloc(sizeof(lists_t));
+	new = malloc(sizeof(list_t));
 	if (!new)
 		return (free_all(head), NULL);
 	if (!str)
