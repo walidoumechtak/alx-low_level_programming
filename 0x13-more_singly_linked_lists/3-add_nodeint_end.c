a241bd30decc58313c179902d09d04c4827e5203
walid oumechtak <woumechtak@gmail.com>
2023-08-01 07:40:21 -0700

    done

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,9 +17,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
-	if (*head == NULL)
+	if (*head == NULL || *head = NULL)
 	{
-		*head = new;
+		head = &new;
 		return (new);
 	}
 	temp = *head;
