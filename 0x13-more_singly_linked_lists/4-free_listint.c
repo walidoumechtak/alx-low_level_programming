fa9dc9c308e0962cdb61579f0183b8330fada309
walid oumechtak <woumechtak@gmail.com>
2023-08-01 08:09:46 -0700

    done

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - free a linked list
+ * @head: the head of the linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t	*temp;
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
