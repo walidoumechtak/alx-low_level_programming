2d2fa2b4e0403a4ae46e7a1fd95e5001256c6599
walid oumechtak <woumechtak@gmail.com>
2023-08-03 07:54:32 -0700

    done

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,16 +10,24 @@ void print_binary(unsigned long int n)
 	int	num_bits;
 	int	i;
 	unsigned long	int	mask;
+	int	flag;
 
-	numb_bits = 8 * sizeof(unsigned long int);
+	flag = 0;
+	num_bits = 8 * sizeof(unsigned long int);
 	i = num_bits;
 	while (i - 1 >= 0)
 	{
 		mask = 1UL << i;
 		if (n & mask)
+		{
 			printf("1");
-		else
+			flag = 1;
+		}
+		else if (flag)
 			printf("0");
+		i--;
 	}
+	if (!flag)
+		printf("0");
 	printf("\n");
 }
