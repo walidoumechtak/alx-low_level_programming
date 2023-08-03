12b9742edf0c960e2e69293786c2722527376cbe
walid oumechtak <woumechtak@gmail.com>
2023-08-03 00:53:18 -0700

    done

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,6 +1,27 @@
 #include "main.h"
 
 /**
+ * check_all_string - check if all string is containe only digit
+ * @s: the string to check
+ * Return: 0 if not or 1 if there is a no digit
+ */
+
+int	check_all_string(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (isdigit(s[i]) == 0)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/**
  * binary_to_uint - func that converts a binary number to an unsigned int
  * @b: the binarya number
  * Return: the conveted number
@@ -8,5 +29,6 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-
+	if (!b || check_all_string(b) == 0)
+		return (0);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
 
 unsigned int binary_to_uint(const char *b);
 
