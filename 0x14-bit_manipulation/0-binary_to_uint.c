c0324ea956a529e0f4e8d9efd745ddf152fbb8dd
walid oumechtak <woumechtak@gmail.com>
2023-08-03 00:46:06 -0700

    done

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - func that converts a binary number to an unsigned int
+ * @b: the binarya number
+ * Return: the conveted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
+}
