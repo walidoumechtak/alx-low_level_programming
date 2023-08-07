4a30964a3c6d15c2b0183d9e285352b3e7831e5b
walid oumechtak <woumechtak@gmail.com>
2023-08-07 08:21:32 -0700

    remove the main test

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,17 +22,3 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	return (1);
 }
-
-int main(int ac, char **av)
-{
-    int res;
-
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
-}
