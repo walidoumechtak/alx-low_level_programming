ecbeeaac78f73842902979a9d29f8e462312bb00
walid oumechtak <woumechtak@gmail.com>
2023-08-08 01:33:13 -0700

    corr 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	len = strlen(text_content);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
-		return (-1);
+		return (1);
 	if (write(fd, text_content, len) < 0)
 		return (-1);
 	return (1);
