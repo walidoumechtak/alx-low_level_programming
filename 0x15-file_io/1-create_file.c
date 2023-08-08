8e01c94af9da8400280f755c23ea1d265318e463
walid oumechtak <woumechtak@gmail.com>
2023-08-08 01:35:36 -0700

    add some small shcekc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,12 +12,14 @@ int create_file(const char *filename, char *text_content)
 	int	fd;
 	int	len;
 
-	if (!text_content)
-		return (-1);
 	len = strlen(text_content);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd < 0)
-		return (1);
+	{
+		if (!text_content)
+			return (1);
+		return (-1);
+	}
 	if (write(fd, text_content, len) < 0)
 		return (-1);
 	return (1);
