8ae7d767d6a0f1a31bf489f9c838359709f303ea
walid oumechtak <woumechtak@gmail.com>
2023-09-14 03:32:28 -0700

    done with the exe

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * sum_dlistint - calc the sum of all data n in DLL
  * @head: the head of DLL
- * Retrun: the sum of all n DLL
+ * Return: the sum of all n DLL
  */
 
 int sum_dlistint(dlistint_t *head)
