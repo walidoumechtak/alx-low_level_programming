5e2f2ec996a5c5a09b6cacf23989f482fe6bc11a
walid oumechtak <woumechtak@gmail.com>
2023-09-13 23:41:05 -0700

    done with the exe

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t	cpt;
+
+	cpt = 0;
+	while (h)
+	{
+		printf("%d\n", h->n);
+		cpt++;
+		h = h->next;
+	}
+	return (cpt);
+}
