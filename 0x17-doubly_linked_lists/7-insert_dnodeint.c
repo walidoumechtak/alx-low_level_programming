e31faa2e3ff2e8f875d5031e9d525707b4a37478
walid oumechtak <woumechtak@gmail.com>
2023-09-14 02:29:35 -0700

    done with the exe

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at index idx
+ * @h: the addr of the head
+ * @idx: the place where to insert the new node
+ * @n: the value of data n inside the node
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t	*temp;
+	unsigned int	i;
+	dlistint_t	*new;
+	dlistint_t	*pv;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	temp = *h;
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	i = 0;
+	if (!h || !*h)
+		return (NULL);
+	else
+	{
+		while (temp->next)
+		{
+			if (i == idx)
+				break;
+			i++;
+			temp = temp->next;
+		}
+		pv = temp->prev;
+		new->next = temp;
+		new->prev = temp->prev;
+		pv->next = new;
+		temp->prev = new;
+	}
+	return (new);
+}
--- 0x17-doubly_linked_lists/main.c
+++ 0x17-doubly_linked_lists/main.c
@@ -10,8 +10,7 @@
  */
 int main(void)
 {
-	 dlistint_t *head;
-    int sum;
+    dlistint_t *head;
 
     head = NULL;
     add_dnodeint_end(&head, 0);
@@ -22,9 +21,11 @@ int main(void)
     add_dnodeint_end(&head, 98);
     add_dnodeint_end(&head, 402);
     add_dnodeint_end(&head, 1024);
-    sum = sum_dlistint(head);
-    printf("sum = %d\n", sum);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
     free_dlistint(head);
     head = NULL;
-	return (EXIT_SUCCESS);
+    return (EXIT_SUCCESS);
 }
