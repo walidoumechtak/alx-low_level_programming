8afa785d1b1878858906f202eaf319ac99910eac
walid oumechtak <woumechtak@gmail.com>
2023-09-14 00:15:10 -0700

    done with the exe

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - add node at the beginning of DDL
+ * @head: the addr of the head of DLL
+ * @n: the content of the new linkedlist
+ * Return: the new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t	*new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return NULL;
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (!head)
+		head = &new;
+	else
+	{
+		new->next = *head;
+		*head = new;
+	}
+	return (new);
+}
