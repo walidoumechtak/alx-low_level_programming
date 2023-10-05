85680c8492ae6a46e5f793809d7291120e74a40e
walid oumechtak <woumechtak@gmail.com>
2023-10-05 08:22:34 -0700

    done

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_table.hpp"
+
+/**
+ * hash_table_create - create a hash table
+ * @size: the size of hash table
+ * Return: return the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+		return (NULL);
+	hash->array = malloc(sizeof(hash_node_t) * size);
+	if (!hash->array)
+	{
+		free(hash);
+		return (NULL);
+	}
+	return (hash);
+}
