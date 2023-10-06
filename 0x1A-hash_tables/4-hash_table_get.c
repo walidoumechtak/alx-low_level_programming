4b61ac1c66eef15ace47b83ff9437bb6d7e19c54
walid oumechtak <woumechtak@gmail.com>
2023-10-06 07:10:49 -0700

    done

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - get the pair with the key
+ * @ht: the hash table
+ * @key: the key to get
+ * Return: return the value from the given key
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	return (NULL);
+}
