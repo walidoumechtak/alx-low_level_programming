ec1cf83f112d2d0c602012bd343ba03fc185bbdb
walid oumechtak <woumechtak@gmail.com>
2023-10-06 02:39:45 -0700

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,7 +11,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash;
 
 	hash = malloc(sizeof(hash_table_t));
-	if (!hash)
+	if (!hash || size <= 0)
 		return (NULL);
 	hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (!hash->array)
